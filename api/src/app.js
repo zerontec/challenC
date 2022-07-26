589afeeff9f6878113bd52d0dc8f67ee8aea4197
zerontec <zerontecnologia1@gmail.com>
2022-07-26 16:34:03 -0400

    I did app file

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index');
@@ -16,17 +17,25 @@ server.options('*', cors(
 ));
 
 
-/* require('./db.js');  */
+
 
 
 server.name = 'API';
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   
-
+  // // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  // res.header('Access-Control-Allow-Origin', '*'); // habilita a cualquir dire front
+  // res.header('Access-Control-Allow-Credentials', 'true');
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  // res.header("Access-Control-Allow-Headers","x-access-token, Origin, Content-Type, Accept"
+  // );
+  // next();
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -34,8 +43,6 @@ server.use((req, res, next) => {
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 }); 
-
-
 
 server.use('/', routes);
 
@@ -46,8 +53,6 @@ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   console.error(err);
   res.status(status).send(message);
 });
-
-
 
 module.exports = server;
 
