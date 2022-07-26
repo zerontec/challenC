6abc347bbdeec5262916573f884b3a1293b3b50e
zerontec <zerontecnologia1@gmail.com>
2022-07-26 16:34:29 -0400

    I did routes file

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,12 @@
 const {Router} = require ('express');
 const router = Router();
+const palindromeRoutes = require ('./echoTextRoutes');
 
-router.get('/', (req, res) => { 
-res.status(200).send('Hello World!') });
 
+// router.get('/', (req, res) => { 
+//     res.status(200).send('Hello World!') });
+
+  router.use('/iecho', palindromeRoutes );
+  
+  
 module.exports = router;
