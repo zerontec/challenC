fed36d33d77c478ab387127fcc39b4ff5483cfac
zerontec <zerontecnologia1@gmail.com>
2022-07-26 22:28:13 -0400

    I did changes in controler palindrome

--- api/src/controller/echoText.controller.js
+++ api/src/controller/echoText.controller.js
@@ -1,34 +1,40 @@
 
 
+/**
+ * It takes a string, converts it to lowercase, reverses it, and then compares it to the original
+ * string. If they are the same, it returns a message saying it's a palindrome. If they are not the
+ * same, it returns a message saying it's not a palindrome.
+ * @param req - The request object. This object represents the HTTP request and has properties for the
+ * request query string, parameters, body, HTTP headers, and so on.
+ * @param res - The response object.
+ * @param next - A function that you call to pass control to the next middleware function.
+ * @returns a promise.
+ */
 
-async function palindrome(req,res, next){
 
-try{
 
-   let text = req.query.text;
-   
-   const lowerCase= text.toLowerCase();
+async function palindrome(req, res, next) {
+  try {
+    let text = req.query.text;
 
-   const reverseText = lowerCase.split('').reverse().join('');
-   if(lowerCase === reverseText){
 
-    res.status(200).send({message:`text: ${reverseText} es Palindrome`})
 
-   }else{
-    return res.status(400).send({message:`text: ${reverseText} no es Palindrome`})
-   }
+    const lowerCase = text.toLowerCase();
 
-}catch(err){
-  res.status(500).send({err})
+    const reverseText = lowerCase.split("").reverse().join("");
+    if (lowerCase === reverseText) {
+      res.status(200).send({ message: `text: ${reverseText}   es Palindrome` });
+    } else {
+      return res
+        .status(200)
+        .send({ message: `text: ${reverseText} no es Palindrome` });
+    }
+  } catch (err) {
+    res.status(500).send({ err });
+    console.log(err)
+  }
 }
 
-
-
-}
-
-
-module.exports ={
-
-    palindrome
-
-}
+module.exports = {
+  palindrome,
+};
