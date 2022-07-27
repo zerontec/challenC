3248db87c2248a1a6e9394fdbb4816c9b5089dd0
zerontec <zerontecnologia1@gmail.com>
2022-07-26 22:28:50 -0400

    I did changes in routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,8 +3,7 @@ const router = Router();
 const palindromeRoutes = require ('./echoTextRoutes');
 
 
-// router.get('/', (req, res) => { 
-//     res.status(200).send('Hello World!') });
+
 
   router.use('/iecho', palindromeRoutes );
   
